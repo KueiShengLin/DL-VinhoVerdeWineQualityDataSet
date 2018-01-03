ed7ccae6ad7607a8f3847ccc424fb1b8f28ffec3
Tim Lin <tarry21304@gmail.com>
2018-01-03 11:53:43 +0800

    LR/KNN/ANN score

--- wqLR.py
+++ wqLR.py
@@ -5,7 +5,8 @@ from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.cluster import KMeans
-
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
 
 def preprocess():
     dataset = pd.read_csv("./dataset/winequality-red-dot.csv")
@@ -29,18 +30,37 @@ del test_data['quality']
 
 clf = LogisticRegression(multi_class='multinomial', penalty='l2', solver='sag', tol=0.01)
 clf.fit(train_data.values, train_y)
+prediction = clf.predict(test_data.values)
 score = clf.score(test_data.values, test_y)
+recall = recall_score(test_y, prediction, average="macro")
+f1 = f1_score(test_y, prediction, average="macro")
 print("lr score:", score)
+print("lr recall:", recall)
+print("lr f1:", f1)
 
 knn = KNeighborsClassifier()
 knn.fit(train_data.values, train_y)
+knn_prediction = knn.predict(test_data.values)
 knn_score = knn.score(test_data.values, test_y)
+knn_recall = recall_score(test_y, knn_prediction, average="macro")
+knn_f1 = f1_score(test_y, knn_prediction, average="macro")
+print('')
 print("knn score:", knn_score)
+print("knn recall:", knn_recall)
+print("knn f1:", knn_f1)
 
-kmean = KMeans(n_clusters=6)
-kmean.fit(train_data.values, train_y)
-kmean_score = kmean.score(test_data, test_y)
-print("kmean score:", kmean_score)
+ann_prediction = np.loadtxt("./prediction/test_prediction.txt", delimiter=',')
+dl_recall = recall_score(test_y, ann_prediction, average="macro")
+dl_f1 = f1_score(test_y, ann_prediction, average="macro")
+print('')
+print("dl score:", 0.65)
+print("dl recall:", dl_recall)
+print("dl f1:", dl_f1)
+
+# kmean = KMeans(n_clusters=6)
+# kmean.fit(train_data.values, train_y)
+# kmean_score = kmean.score(test_data, test_y)
+# print("kmean score:", kmean_score)
 
 #
 
